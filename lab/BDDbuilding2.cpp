733be9a8b4e9b73a34252bbc433e92d3a72ce5b2
iamfsx <1478077519@qq.com>
2025-05-24 23:17:02 +0800

    modified:   lab/BDDbuilding2.cpp

--- lab/BDDbuilding2.cpp
+++ lab/BDDbuilding2.cpp
@@ -5,76 +5,70 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
+#include <random>
+#include <cassert>
 #include "cuddObj.hh"
 #include "cuddInt.h"
 
 std::unordered_map<DdNode*, __float128> node_odd_cnt;
 std::unordered_map<DdNode*, __float128> node_even_cnt;
 
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::uniform_real_distribution<double> dis(0.0, 1.0);
+
 std::pair<__float128,__float128> countPaths(DdManager* mgr, DdNode* n){
+    
     auto it = node_even_cnt.find(n);
     if(it != node_even_cnt.end()){
         return std::make_pair(node_odd_cnt[n], node_even_cnt[n]);
     }
+
     if(Cudd_IsConstant(n)){
-        return Cudd_IsComplement(n) ?
-            std::pair<__float128,__float128>{0, 0} :
-            std::pair<__float128,__float128>{0, 1};
+        __float128 odd = 0, even = 0;
+        if(Cudd_IsComplement(n)) odd = 1;
+        else even = 1;
+        node_odd_cnt[n] = odd;
+        node_even_cnt[n] = even;
+        return std::make_pair(odd, even);
     }
 
     DdNode* real = Cudd_Regular(n);
     bool is_complement = Cudd_IsComplement(n);
     DdNode* t = Cudd_T(real);
     DdNode* e = Cudd_E(real);
-    if(is_complement){
-        t = Cudd_Not(t);
-        e = Cudd_Not(e);
-    }
+
     auto [odd_t, even_t] = countPaths(mgr, t);
     auto [odd_e, even_e] = countPaths(mgr, e);
-    __float128 odd = 0, even = 0;
-
-    if(Cudd_IsComplement(t)){
-        odd += even_t;
-        even += odd_t;
-    } else {
-        odd += odd_t;
-        even += even_t;
-    }
-    if(Cudd_IsComplement(e)){
-        odd += even_e;
-        even += odd_e;
-    } else {
-        odd += odd_e;
-        even += even_e;
-    }
-
+    __float128 odd = odd_t + odd_e;
+    __float128 even = even_t + even_e;
+    if(is_complement)
+        std::swap(odd, even);
     node_odd_cnt[n] = odd;
     node_even_cnt[n] = even;
     return std::make_pair(odd, even);
 }
 
-void DFS(DdNode* x,bool odd,std::vector<int>& path){
+void DFS(DdNode* x,int odd,std::vector<int>& path){
     
-    if(Cudd_IsConstant(x))
+    if(Cudd_IsConstant(x)){
+        assert(!odd);
         return;
-    
+    }
     DdNode* real = Cudd_Regular(x);
     int var_idx = Cudd_NodeReadIndex(real);
-    bool is_complement = Cudd_IsComplement(x);
     DdNode* t = Cudd_T(real);
     DdNode* e = Cudd_E(real);
-    if(is_complement){
-        t = Cudd_Not(t);
-        e = Cudd_Not(e);
-    }
-
-    bool odd_left = odd ^ Cudd_IsComplement(t);
-    bool odd_right = odd ^ Cudd_IsComplement(e);
-    __float128 cnt_left = odd_left ? node_odd_cnt[t] : node_even_cnt[t];
-    __float128 cnt_right = odd_right ? node_odd_cnt[e] : node_even_cnt[e];
-    double prob_left = (double)(cnt_left / (cnt_left + cnt_right));
-    if((double)rand() / RAND_MAX < prob_left){
+   
+    int odd_left = odd ^ Cudd_IsComplement(t);
+    int odd_right = odd ^ Cudd_IsComplement(e);
+    __float128 cnt_left = odd ? node_odd_cnt[t] : node_even_cnt[t];
+    __float128 cnt_right = odd ? node_odd_cnt[e] : node_even_cnt[e];
+    double prob_left = 0.5;
+    if(cnt_left + cnt_right > 0)
+        prob_left = static_cast<double>(cnt_left) / (cnt_left + cnt_right);
+    
+    if(dis(gen) < prob_left){
         path.push_back(var_idx);
         DFS(t, odd_left, path);
     } else {
@@ -103,13 +97,13 @@ int main(int argc, char* argv[]) {
         int input;
         fin >> input;
         int idx = input / 2;
-        bdd_vars[idx] = Cudd_bddIthVar(mgr, idx);
+        bdd_vars[idx] = Cudd_bddIthVar(mgr,idx);
         Cudd_Ref(bdd_vars[idx]);
     }
     //L = 0
     //O = 1
-    int output;
-    fin >> output;
+    int output_idx;
+    fin >> output_idx;
 
     Cudd_AutodynEnable(mgr, CUDD_REORDER_GROUP_SIFT);
 
@@ -118,44 +112,35 @@ int main(int argc, char* argv[]) {
         fin >> lhs >> rhs0 >> rhs1;
         int id_lhs = lhs / 2;
         
-        DdNode* f0 = bdd_vars[rhs0/2];
+        DdNode* f0 = bdd_vars[rhs0 / 2];
         if (rhs0 & 1) f0 = Cudd_Not(f0);
-        DdNode* f1 = bdd_vars[rhs1/2];
+        DdNode* f1 = bdd_vars[rhs1 / 2];
         if (rhs1 & 1) f1 = Cudd_Not(f1);
         
-        DdNode* tmp = Cudd_bddAnd(mgr, f0, f1);
-        Cudd_Ref(tmp);
-        
-        if (bdd_vars[id_lhs] != nullptr) {
-            Cudd_RecursiveDeref(mgr, bdd_vars[id_lhs]);
-        }
-        bdd_vars[id_lhs] = tmp;
+        bdd_vars[id_lhs] = Cudd_bddAnd(mgr, f0, f1);
+        Cudd_Ref(bdd_vars[id_lhs]);
     }
-
-    DdNode* output_bdd = bdd_vars[output/2];
-    if (output & 1) output_bdd = Cudd_Not(output_bdd);
+    
+    DdNode* output_bdd = bdd_vars[output_idx / 2];
+    if (output_idx & 1) output_bdd = Cudd_Not(output_bdd);
     Cudd_Ref(output_bdd);
-
+    
     Cudd_AutodynDisable(mgr);
     Cudd_ReduceHeap(mgr, CUDD_REORDER_SIFT, 0);
 
     countPaths(mgr, output_bdd);
-    std::vector<int> path;
-    DFS(output_bdd, Cudd_IsComplement(output_bdd), path);
-
-    sort(path.begin(), path.end(), [](int a, int b) {
-        return abs(a) < abs(b);
-    });
-    for(int var: path){
-        bool val = var > 0;
-        std::cout << "x" << var << " = " << (val ? "1" : "0") << "\n";
+    for(int i=0;i<10;i++){
+        gen.seed(rd() + i);
+        std::vector<int> path;
+        DFS(output_bdd, Cudd_IsComplement(output_bdd), path);
+        for(int var: path) std::cout << var <<" ";
+        std::cout << std::endl; 
     }
     
-    for(auto& bdd_var : bdd_vars) {
-        if (bdd_var != nullptr) {
+    for(auto& bdd_var : bdd_vars) 
+        if (bdd_var != nullptr) 
             Cudd_RecursiveDeref(mgr, bdd_var);
-        }
-    }
+        
     Cudd_Quit(mgr);
     return 0;
 }
